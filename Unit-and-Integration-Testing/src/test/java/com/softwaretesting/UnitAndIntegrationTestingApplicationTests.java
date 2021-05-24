fc9c7dbb20d5ff342b48063b58ce6d0ed5b0b251
Harishankar Bhat R <rhsb1997@gmail.com>
2021-05-24 22:21:30 +0530

    Bootstrapped Project

---
+++ Unit-and-Integration-Testing/src/test/java/com/softwaretesting/UnitAndIntegrationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitAndIntegrationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
